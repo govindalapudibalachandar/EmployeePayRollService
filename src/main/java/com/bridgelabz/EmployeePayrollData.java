15868619c5aeb094e91aab0bf521f54a5421a637
balachandar <govindalapudibalachandar@gmail.com>
2021-09-29 23:00:23 +0530

    Added:EmployeePayrollServiceReadWrite file added

--- src/main/java/com/bridgelabz/EmployeePayrollData.java
+++ src/main/java/com/bridgelabz/EmployeePayrollData.java
@@ -1,8 +1,19 @@
 package com.bridgelabz;
 
-public class EmployeePayrollData {
-
-    public static void main(String[] args) {
-        System.out.println("Welcome to EmployeePayRollService ");
+    public class EmployeePayrollData {
+        //Declare Variables
+        public int id;
+        public String name;
+        public double salary;
+        //Constructor
+        public EmployeePayrollData(int id, String name, double salary) {
+            this.id = id;
+            this.name = name;
+            this.salary = salary;
+        }
+        //To String Method
+        @Override
+        public String toString() {
+            return "EmployeePayrollData [id=" + id + ", name=" + name + ", salary=" + salary + "]";
+        }
     }
-}
