69d4ba8214136279e9f57f527494e0f76c90295a
balachandar <govindalapudibalachandar@gmail.com>
2021-09-29 22:29:36 +0530

    Added:EmployeePayRollService file is added

---
+++ src/main/java/com/bridgelabz/EmployeePayrollData.java
@@ -0,0 +1,8 @@
+package com.bridgelabz;
+
+public class EmployeePayrollData {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to EmployeePayRollService ");
+    }
+}
